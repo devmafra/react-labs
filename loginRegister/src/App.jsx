02e4f33dcd8e507a2a88d8609d79af150ea6a550
Davi Mafra <dev@davimafra.com>
2026-04-16 22:18:37 -0300

    feat(router): define initial auth routes for Login and Register

--- loginRegister/src/App.jsx
+++ loginRegister/src/App.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { AppRoutes } from "./routes/AppRoutes";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <AppRoutes />
+    </>
+  );
 }
