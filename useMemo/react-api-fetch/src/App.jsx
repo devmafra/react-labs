d29544b054fb8a0949097c4a1cf94d38c63a85ec
Davi Mafra <dev@davimafra.com>
2026-05-09 19:14:03 -0300

    study: refatoração da requisição api para axios com tratamento de erro granular

--- useMemo/react-api-fetch/src/App.jsx
+++ useMemo/react-api-fetch/src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
   const [dados, setDados] = useState(null);
 
+  // * Requisição via THEN/CATCH
   // const handleFetchData = async () => {
   //   return fetch("https://jsonplaceholder.typicode.com/posts/1")
   //     .then((response) => {
@@ -31,36 +33,65 @@ function App() {
   //     });
   // };
 
+  // * Requisição via TRY/CATCH
+  // const handleFetchData = async () => {
+  //   try {
+  //     const response = await fetch(
+  //       "https://jsonplaceholder.typicode.com/postws/1",
+  //     );
+
+  //     if (!response.ok) {
+  //       throw new Error("Erro HTTP!", {
+  //         cause: { status: response.status },
+  //       });
+  //     }
+
+  //     const json = await response.json();
+
+  //     setDados(json);
+
+  //     return json;
+  //   } catch (error) {
+  //     console.error("Erro ao buscar os dados", error);
+
+  //     const status = error.cause?.status;
+
+  //     setDados({
+  //       title: error.message,
+  //       body:
+  //         status === 404 ? "Erro 404 - Dados não encontrados": "Ocorreu um erro inesperado. Tente novamente mais tarde.",
+  //     });
+  //     return null;
+  //   }
+  // };
+
+  // * Requisição via AXIOS
   const handleFetchData = async () => {
     try {
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/postws/1",
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/post2s/1",
       );
-
-      if (!response.ok) {
-        throw new Error("Erro HTTP!", {
-          cause: { status: response.status },
-        });
-      }
-
-      const json = await response.json();
-
-      setDados(json);
-
-      return json;
+      setDados(response.data);
     } catch (error) {
-      console.error("Erro ao buscar os dados", error);
-
-      const status = error.cause?.status;
-
-      setDados({
-        title: error.message,
-        body:
-          status === 404
-            ? "Erro 404 - Dados não encontrados"
-            : "Ocorreu um erro inesperado. Tente novamente mais tarde.",
-      });
-      return null;
+      if (error.response) {
+        console.log(error.message);
+        console.log(error.response.status);
+        setDados({
+          title: `Erro ${error.response.status}!`,
+          body:
+            error.response.status === 404
+              ? "Dados não encontrados"
+              : "Erro no servidor.",
+        });
+      } else if (error.request) {
+        console.log(error.request);
+        setDados({
+          title: "Erro de Conexão",
+          body: "Não foi possível alcançar o servidor. Verifique sua internet.",
+        });
+      } else {
+        console.log("Error Message", error.message);
+      }
     }
   };
 
